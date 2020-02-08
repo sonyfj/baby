659956204db2062904ce9ea2b6d94a69297bbf6e
Sony Jose <sonyfj@Sonys-Mac-mini.local>
2020-02-07 23:27:49 -0800

    fix styles

--- src/App.js
+++ src/App.js
@@ -17,22 +17,22 @@ function App() {
         height={height}
       />
       <div width="100%" style={{paddingTop: "60px"}}>
-        <div style={{margin: "0 auto", width: "700px"}}>
-        <img src="http://c2.staticflickr.com/2/1596/23948188374_77061251e2_z.jpg"
-            style={{marginLeft: '-70px'}}>
-        </img>
-        <div style={{marginTop: "-200px"}}>
-        <div style={{display: "flex", width: "700px",
-              justifyContent: "space-between"}} >
-              <div>
-                <img width="250px" src={girl} />
-              </div>
-              <div>
-                <img width="250px" src={boy} />
+        <div style={{margin: "0 auto", width: "300px"}}>
+          <img src="http://c2.staticflickr.com/2/1596/23948188374_77061251e2_z.jpg"
+              style={{marginLeft: '-40px', width: "250px"}}>
+          </img>
+          <div style={{marginTop: "-120px"}}>
+          <div style={{display: "flex", width: "300",
+                justifyContent: "space-between"}} >
+                <div>
+                  <img width="120px" src={girl} />
+                </div>
+                <div>
+                  <img width="120px" src={boy} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
       </div>
     </div>
   );
